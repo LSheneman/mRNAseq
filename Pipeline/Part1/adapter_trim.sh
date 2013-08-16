e693cc0a9c350ee17e8905af0ce41a6bc21d3bb4
LSheneman <leighs@msu.edu>
2013-08-15 23:09:39 -0400

    ...

--- Pipeline/Part1/adapter_trim.sh
+++ Pipeline/Part1/adapter_trim.sh
@@ -1,6 +1,6 @@
 python /usr/local/share/khmer/sandbox/write-trimmomatic.py > trim.sh
 	touch top-output.txt
-	while [! -f time/adapter_trim.sh.time]
+	while [ ! -f time/adapter_trim.sh.time]
 	do
 		sar -u ALL >> top-output.txt
 		sleep 5
