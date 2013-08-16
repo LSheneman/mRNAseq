dea2281ed9d15203d04273588bf6d0a354ed4bf3
LSheneman <leighs@msu.edu>
2013-08-15 22:23:31 -0400

    ...

---
+++ Pipeline/Part1/adapter_trim.sh
@@ -0,0 +1,6 @@
+python /usr/local/share/khmer/sandbox/write-trimmomatic.py > trim.sh
+	touch top-output.txt
+	until [[-f adapter_trim.sh.time]]; do
+		top -n 1 -b >> top-output.txt
+		sleep 300
+	done
