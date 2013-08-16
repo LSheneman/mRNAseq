77b7dace1836e83f3085ac4d30ccec10cfd9af67
LSheneman <leighs@msu.edu>
2013-08-16 11:02:56 -0400

    attempting to get CPU stats

--- Pipeline/Part1/adapter_trim.sh
+++ Pipeline/Part1/adapter_trim.sh
@@ -2,6 +2,6 @@ python /usr/local/share/khmer/sandbox/write-trimmomatic.py > trim.sh
 	touch top-output.txt
 	while [ ! -f time/adapter_trim.sh.time ]
 	do
-		sar -u ALL 1 1 >> cpu.txt
+		mpstat -P all >> top-output.txt
 		sleep 5
 	done
