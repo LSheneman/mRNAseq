db10b00d2744dd83f6d813e83582a6e4096b9502
LSheneman <leighs@msu.edu>
2013-08-17 18:56:38 -0400

    ...

--- Pipeline/Part2/diginorm_pe.sh
+++ Pipeline/Part2/diginorm_pe.sh
@@ -1,3 +1,5 @@
+KHMER=/usr/local/share/khmer
+
 python ${KHMER}/scripts/normalize-by-median.py -p -k 20 -C 20 -N 4 -x 3e9 --savehash normC20k20.kh *.pe.qc.fq.gz &
 touch diginorm_pe.sh.mp.txt , diginorm_pe.sh.vm.txt
 until [ -f diginorm_pe.sh.time ]
