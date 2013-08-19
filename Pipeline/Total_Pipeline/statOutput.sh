50c0230569a20b631a4d507f1b6812071330b08c
LSheneman <leighs@msu.edu>
2013-08-19 17:06:53 -0400

    troubleshooting

--- Pipeline/Total_Pipeline/statOutput.sh
+++ Pipeline/Total_Pipeline/statOutput.sh
@@ -2,7 +2,7 @@
 touch $1.mp.txt , $1.vm.txt
 until [ -f ../time/$1 ]
 do
-	mpstat -P ALL 1 2 | tail -6 >> output/$1.mp.txt
-	vmstat 1 2 | (head -2; tail -1) | tail -2 >> output/$1.vm.txt
+	mpstat -P ALL 1 2 | tail -6 >> ../output/$1.mp.txt
+	vmstat 1 2 | (head -2; tail -1) | tail -2 >> ../output/$1.vm.txt
 	sleep 5
 done
