ae57768c9dc21c9a25836df17e5cdbf1cc24bbc0
LSheneman <leighs@msu.edu>
2013-08-20 12:56:01 -0400

    attempting to get CPU stats

--- Pipeline/Total_Pipeline/adapter_trim.sh
+++ Pipeline/Total_Pipeline/adapter_trim.sh
@@ -3,11 +3,16 @@ progress()
 /usr/bin/time -o ../time/adapter_trim.time python /usr/local/share/khmer/sandbox/write-trimmomatic.py > trim.sh
 }
 
+stats()
+{
 touch adapter_trim.mp.txt , adapter_trim.vm.txt
-until [ -f ../time/adapter_trim.time ]
-do
-	mpstat -P ALL 1 2 | tail -6 >> ../output/adapter_trim.mp.txt
-	vmstat 1 2 | (head -2; tail -1) | tail -2 >> ../output/adapter_trim.vm.txt
-	sleep 5
-done & progress()
-done
+        until [ -f ../time/adapter_trim.time ]
+        do
+                mpstat -P ALL 1 2 | tail -6 >> ../output/adapter_trim.mp.txt
+                vmstat 1 2 | (head -2; tail -1) | tail -2 >> ../output/adapter_trim.vm.txt
+                sleep 5
+        done
+}
+
+coproc stats progess
+echo "done"
