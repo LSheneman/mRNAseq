ee090f8e2ae6bee8413ad3880f3ee10c525c4bb2
LSheneman <leighs@msu.edu>
2013-08-19 23:44:53 -0400

    troubleshooting

--- Pipeline/Total_Pipeline/adapter_trim.sh
+++ Pipeline/Total_Pipeline/adapter_trim.sh
@@ -3,11 +3,10 @@ progress()
 /usr/bin/time -o ../time/adapter_trim.time python /usr/local/share/khmer/sandbox/write-trimmomatic.py > trim.sh
 }
 
-progress() &
 touch adapter_trim.mp.txt , adapter_trim.vm.txt
 until [ -f ../time/adapter_trim.time ]
 do
 	mpstat -P ALL 1 2 | tail -6 >> ../output/adapter_trim.mp.txt
 	vmstat 1 2 | (head -2; tail -1) | tail -2 >> ../output/adapter_trim.vm.txt
 	sleep 5
-done
+done & progress()
