aea9148b576e25fdda5fd9138945938ed47e64cd
LSheneman <leighs@msu.edu>
2013-08-27 21:36:32 -0400

    Added Trinity Protocol, reorganized, fixed loop in stats.sh

--- Pipeline/get_mouse_info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Setup mouse reference info
-python /usr/local/share/eel-pond/make-namedb.py mouse.protein.faa mouse.namedb
-python -m screed.fadbm mouse.protein.faa
--- source/khmer_install.sh
+++
@@ -1,9 +0,0 @@
-#Install Khmer
-
-cd /usr/local/share
-git clone https://github.com/ged-lab/khmer.git -b bleeding-edge
-cd khmer
-make
-
-echo 'export PYTHONPATH=/usr/local/share/khmer/python' >> ~/.bashrc
-source ~/.bashrc
