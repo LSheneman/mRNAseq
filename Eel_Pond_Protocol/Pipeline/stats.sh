5ce8161d458864e7be3e5918916a5b9662af4c61
LSheneman <leighs@msu.edu>
2013-08-27 23:26:33 -0400

    minor fix

--- Eel_Pond_Protocol/Pipeline/stats.sh
+++ Eel_Pond_Protocol/Pipeline/stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 touch $1.vm.txt $1.mp.txt
-until [ -f $1.time ]
+until [ -f $1.time.done ]
 do 
     mpstat -P ALL 1 2 | tail -6 >> $1.mp.txt
     vmstat 1 2 | (head -2; tail -1) | tail -2 >> $1.vm.txt
