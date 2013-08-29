e3a737a69ff3b81b0a9deaa76d425c02cecc0584
LSheneman <leighs@msu.edu>
2013-08-29 12:25:32 -0400

    added software install

---
+++ Trintiy_Nature_Protocol/source/r_para.R
@@ -0,0 +1,7 @@
+source( "http://bioconductor.org/biocLite.R"	)
+biocLite()
+biocLite (' )edgeR ' )
+biocLite(' )ctc ' )
+biocLite(' )Biobase ' )
+biocLite(' )ape ' )
+install.packages(' )gplots ' )
---
+++ Trintiy_Nature_Protocol/source/screed_install.sh
@@ -0,0 +1,6 @@
+#Script to install screed
+cd /usr/local/share
+
+git clone https://github.com/ged-lab/screed.git
+cd screed
+python setup.py install
