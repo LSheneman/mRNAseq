c3c40672c5c2992292d65d392561cd33ca40703d
LSheneman <leighs@msu.edu>
2013-09-24 14:44:45 -0400

    updating software install files.

--- source/TP_Nema_Setup1/Pipeline/install_software.sh
+++ source/TP_Nema_Setup1/Pipeline/install_software.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #general packages
 apt-get install sysstat --force-yes 
-SOFTWARE_SOURCE=/root/mRNAseq/source
+apt-get install r-base --force-yes 
+SOFTWARE_SOURCE=/root/mRNAseq/Source
 
 #Install eel-pond scripts
 git clone https://github.com/ctb/eel-pond.git /usr/local/share/eel-pond
@@ -11,12 +12,8 @@ cd /mnt
 mkdir work && cd work
 curl -O https://s3.amazonaws.com/public.ged.msu.edu/trinity-nematostella-raw.fa.gz
 
-${SOFTWARE_SOURCE}/screed_install.sh
-${SOFTWARE_SOURCE}/khmer_install.sh 
-${SOFTWARE_SOURCE}/trimmomatic_install.sh
-${SOFTWARE_SOURCE}/libgtextutils_install.sh
-${SOFTWARE_SOURCE}/fastx_install.sh
 ${SOFTWARE_SOURCE}/trinity_install.sh
 ${SOFTWARE_SOURCE}/bowtie_install.sh
 ${SOFTWARE_SOURCE}/samtools_install.sh
-${SOFTWARE_SOURCE}/blast_install.sh
+${SOFTWARE_SOURCE}/blast.sh
+${SOFTWARE_SOURCE}/r_para.R
