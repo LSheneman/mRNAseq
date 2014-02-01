9d6db1af777ae242a1e33d1b6dff11fcfc2e5dec
Leigh Sheneman <leighs@msu.edu>
2014-02-01 18:58:44 -0500

    updat installs

--- Eel_Pond_Protocol/Pipeline/install_software.sh
+++ Eel_Pond_Protocol/Pipeline/install_software.sh
@@ -3,15 +3,17 @@
 apt-get -y install sysstat 
 apt-get -y install libgtextutils-dev
 SOFTWARE_SOURCE=/root/mRNAseq/source
+chmod 777 ${SOFTWARE_SOURCE}/blast_install.sh 
+chmod 777 ${SOFTWARE_SOURCE}/rsem_install.sh 
 
 #Install eel-pond scripts
 git clone https://github.com/ctb/eel-pond.git /usr/local/share/eel-pond
 
-#Get nema data
+#Get test data
 cd /mnt
 mkdir work 
 cd work
-curl -O https://s3.amazonaws.com/public.ged.msu.edu/trinity-nematostella-raw.fa.gz
+curl -O http://athyra.idyll.org/~t/mrnaseq-subset.tar
 
 ${SOFTWARE_SOURCE}/screed_install.sh 
 ${SOFTWARE_SOURCE}/khmer_install.sh 
